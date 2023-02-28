5c2404d2886d1fe043724996d9858b79f8b74699
Oleksiy Biletskiy <iboobel@gmail.com>
2023-02-28 10:31:07 +0200

    Add URLs

--- news/urls.py
+++ news/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'news'
 urlpatterns = [
     path('hello', views.hello, name='hello'),
+    path('category/all', views.all_categories, name='all_categories'),
+    path('category/<int:category_id>', views.category, name='category'),
+    path('news/<int:news_id>', views.news_detail, name='news_detail'),
 ]
