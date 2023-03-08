ce2a5a4dadad712f72c7fd693151d0963553437a
Oleksiy Biletskiy <iboobel@gmail.com>
2023-03-08 20:39:36 +0200

    Add URLs

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from .models import New, Category
 
-# Create your tests here.
+
+class NewTestCase(TestCase):
+    def setUp(self):
+        Category.objects.create(name='test')
+        New.objects.create(title='test', content='test', categories=Category.objects.get(name='test'))
+
+    def test_new(self):
+        new = New.objects.get(title='test')
+        self.assertEqual(new.categories.name, 'test')
+
+
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        Category.objects.create(name='test 001')
+
+    def test_category(self):
+        category = Category.objects.get(name='test 001')
+        self.assertEqual(category.name, 'test 001')
+
+
+class testViews(TestCase):
+    def setUp(self):
+        Category.objects.create(name='test')
+        New.objects.create(title='test', content='test', categories=Category.objects.get(name='test'))
+
+    def test_hello(self):
+        response = self.client.get('/hello')
+        self.assertEqual(response.status_code, 200)
+
+    def test_all_categories(self):
+        response = self.client.get('/category/all')
+        self.assertEqual(response.status_code, 200)
+
+    def test_category(self):
+        response = self.client.get('/category/1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_news_detail(self):
+        response = self.client.get('/news/1')
+        self.assertEqual(response.status_code, 200)
